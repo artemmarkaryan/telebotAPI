18aeb0efb7aef02e77e2e8eec0f2ff1e04564cef
artemmarkaryan <artemandriy@gmail.com>
2021-04-07 23:57:50 +0300

    sendMessage fix

--- sendMessage.go
+++ sendMessage.go
@@ -1,9 +1,12 @@
 package tgbotbasicapi
 
+import "strconv"
+
 func (bot *Bot) SendMessage(chatId int, text string) (err error){
 	err = bot.sendRequest(
 		"sendMessage",
 		map[string]string{
+			"chat_id": strconv.Itoa(chatId),
 			"text": text,
 		})
 	return
